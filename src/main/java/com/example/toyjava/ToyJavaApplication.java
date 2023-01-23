431834dc03bf50d9af52a099f84919772a6f865c
jinjuwon <drasgon@naver.com>
2023-01-23 17:14:24 +0900

    init setting

---
+++ src/main/java/com/example/toyjava/ToyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.toyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toyjava/ToyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
